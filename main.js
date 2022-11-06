5e6c634f84577c9ed687d48f0983406dbbb703a0
Almuhanad-1 <almuhanad.alnihmy@gmail.com>
2022-11-06 12:31:05 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// const loadMore = () => $.ajax({
+//     type: "GET",
+//     url: "./data.json",
+//     contentType: "application/json; charset=utf-8",
+//     dataType: "json",
+//     success: function(result){
+//         alert(result.d);
+//         console.log(result);
+//     }
+// });
+
+// const loadMore = () => {
+//     fetch('https://api.npoint.io/17a10da3afeb5f21d61e')
+// 	.then(response => response.json())
+// 	.then(data => console.log(data))
+// 	.catch(err => console.error(err));
+// }
